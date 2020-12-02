93987674066cbeef19a4b46e5ce4b94b53f20a13
Valdinei F. Lima <valdinei.lima@yatta.net.br>
2020-12-01 23:00:53 -0300

    validação dos campos

--- src/helpers/fields.ts
+++ src/helpers/fields.ts
@@ -2,8 +2,8 @@ import { FieldDeclaration } from '../types/fields'
 
 export const generateFieldDeclaration = (value: any): FieldDeclaration => {
   return {
-    rules: value.rules || [],
-    reset: value.reset || true,
-    value: value.value || null
+    rules: value.hasOwnProperty('rules') ? value.rules : [],
+    reset: value.hasOwnProperty('reset') ? value.reset : true,
+    value: value.hasOwnProperty('value') ? value.value : value || null
   }
 }
